9abe987d9c0666fd763f3418bb36adae5d015588
ThOM HO <thomho1602@gmail.com>
2022-07-10 18:43:03 +0700

    pagination

--- force-app/main/default/aura/SearchAuraComponent/SearchAuraComponentController.js
+++ force-app/main/default/aura/SearchAuraComponent/SearchAuraComponentController.js
@@ -1,31 +1,86 @@
 ({
-	doInit : function(component, event, helper) {
-		console.log('==component==');
-        helper.getStudents(component);
-	},
-    search: function(component, event, helper) {
-        console.log('==seachItems==');
-        helper.getStudents(component);
-    },
-    newStudent : function(component, event, helper) {
-        component.find('create_student_model').getModalRecordData();
-    },
-    editStudent: function(component, event, helper) {
-        var recordId = event.getSource().get('v.name');
-        console.log('==recordId==' + recordId);
-        component.find('edit_student_modal').getModalRecordData(recordId);
-    },
-    deleteStudent: function(component, event, helper) {
-        var recordId = event.getSource().get('v.name');
-        component.find('confirm_delete_modal').confirmDeleteModal(recordId);
-    },
-    handleReloadEvent: function(component, event, helper) {
-        console.log('==handleReloadEvent==');
-        helper.getStudents(component);
+    init: function (cmp, event, helper) {
+        var actions = [
+            { label: 'Show details', name: 'show_details' },
+            { label: 'Delete', name: 'delete' },
+            { label: 'Update', name: 'update' },
+        ];
+        cmp.set('v.columns', [
+            { label: 'Họ', fieldName: 'HoHocSinh__c', type: 'text' },
+            { label: 'Tên', fieldName: 'TenHocSinh__c', type: 'text' },
+            { label: 'Giới tính', fieldName: 'GioiTinh__c', type: 'text' },
+            { label: 'Ngày Sinh', fieldName: 'NgaySinh__c', type: 'date' },
+            { label: 'Điểm 1', fieldName: 'Diem1__c', type: 'Number' },
+            { label: 'Điểm 2', fieldName: 'Diem2__c', type: 'Number' },
+            { label: 'Điểm 3', fieldName: 'Diem3__c', type: 'Number' },
+            { label: 'Điểm TB', fieldName: 'DiemTB__c', type: 'Number' },
+            { label: 'Tình trạng', fieldName: 'TinhTrang__c', type: 'text' },
+            { type: 'action', typeAttributes: { rowActions: actions } }
+        ]);
+        helper.getStudents(cmp);
+        helper.getCountStudent(cmp);
+        helper.getClass(cmp);
     },
 
-    isCheck:function(component, event, helper) {
-        var recordId = event.getSource().get('v.name');
-        console.log('==checkId==' + recordId);
-    }
-})
+    handleRowAction: function (cmp, event, helper) {
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+
+        switch (action.name) {
+            case 'show_details':
+                var recordId = row.Id;
+                cmp.find('detail_student_modal').getModalRecordData(recordId);
+                break;
+            case 'delete':
+                var recordId = row.Id;
+                cmp.find('confirm_delete_modal').confirmDeleteModal(recordId);
+                break;
+            case 'update':
+                // alert('Showing update: ' + JSON.stringify(row));
+                var recordId = row.Id;
+                console.log('==recordId==' + row.Id);
+                cmp.find('edit_student_modal').getModalRecordData(recordId);
+                break;
+        }
+    },
+
+    search: function(cmp, event, helper) {
+        cmp.set('v.pageCurrent', 1);
+        helper.getStudents(cmp);
+        helper.getCountStudent(cmp);
+    },
+
+    newStudent : function(cmp, event, helper) {
+        cmp.find('create_student_modal').getModalRecordData();
+    },
+
+    handleReloadEvent: function(cmp, event, helper) {
+        console.log('==handleReloadEvent==');
+        helper.getStudents(cmp);
+        helper.getCountStudent(cmp);
+        helper.getClass(cmp);
+    },
+
+    first:function(cmp, event, helper) {
+        cmp.set('v.currentPage', 1);
+        helper.getStudents(cmp);
+    },
+
+    next:function(cmp, event, helper) {
+        cmp.set('v.currentPage', cmp.get('v.currentPage') + 1);
+        console.log('page'+cmp.get('v.currentPage'));
+        helper.getStudents(cmp);
+    },
+
+    previous:function(cmp, event, helper) {
+        cmp.set('v.currentPage', cmp.get('v.currentPage') - 1);
+        console.log('page'+cmp.get('v.currentPage'));
+        helper.getStudents(cmp);
+    },
+
+    last:function(cmp, event, helper) {
+        cmp.set('v.currentPage',  cmp.get('v.totalSize'));
+        helper.getStudents(cmp);
+    },
+    
+});
